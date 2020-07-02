50fc869f87f4fc88855d3c8c6fc2a04b7cf022ae
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-01 20:59:20 -0500

    added the process env to the server js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3002;
+var PORT = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -92,3 +93,5 @@ app.post("/api/characters", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+
